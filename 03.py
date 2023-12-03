b83385bef0d2d0a0171c18fc2ba782c61243b4fe
Greg Sweeney <greg@kittycad.io>
2023-12-03 12:16:31 -0500

    day 3 part 2

--- 03.py
+++ 03.py
@@ -29,6 +29,33 @@ Here is an example engine schematic:
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+
+--- Part Two ---
+
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
 """
 
 from dataclasses import dataclass
@@ -36,6 +63,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Symbol:
+    what: str
     x: int
     y: int
 
@@ -54,7 +82,7 @@ class Number:
         )
 
 
-def part_1(fp: str) -> int:
+def scan_schematic(fp: str) -> tuple[list[Number], list[Symbol]]:
     with open(fp, "r") as f:
         lines = f.readlines()
 
@@ -84,7 +112,13 @@ def part_1(fp: str) -> int:
                 )
                 digit_start = None
             if not digit_start and not c.isdigit() and c != ".":
-                symbols.append(Symbol(x=x, y=y))
+                symbols.append(Symbol(what=c, x=x, y=y))
+
+    return numbers, symbols
+
+
+def part_1(fp: str) -> int:
+    numbers, symbols = scan_schematic(fp)
 
     # check each number for adjacency to symbols
     total = 0
@@ -97,9 +131,34 @@ def part_1(fp: str) -> int:
     return total
 
 
+def part_2(fp: str) -> int:
+    numbers, symbols = scan_schematic(fp)
+
+    # check each * for adjacency to numbers
+    stars = [s for s in symbols if s.what == "*"]
+    total = 0
+    for star in stars:
+        ratio = 1
+        num_adj = 0
+        for number in numbers:
+            if number.is_adjacent(star):
+                ratio *= number.value
+                num_adj += 1
+        if num_adj == 2:
+            total += ratio
+
+    return total
+
+
 if __name__ == "__main__":
     # part 1
     small_ans = part_1("03-01-small.txt")
     assert small_ans == 4361
     print(small_ans)
     print(part_1("03-01-large.txt"))
+
+    # part 2
+    small_ans = part_2("03-01-small.txt")
+    assert small_ans == 467835
+    print(small_ans)
+    print(part_2("03-01-large.txt"))
